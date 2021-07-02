7229b0b8734ffc7a63d86d9ea860bf0ebf958ac3
mauricioapazacallapa <81927407+mauricioapazacallapa@users.noreply.github.com>
2021-07-02 00:57:37 -0400

    Update SeleccionFutbol.java

--- SeleccionFutbol.java
+++ SeleccionFutbol.java
@@ -1,24 +1,30 @@
 public class SeleccionFutbol
 {
 
-	protected int id;
-	protected String nombre;
-	protected String apellidos;
-	protected int edad;
-	
-	  public SeleccionFutbol(int id,String nombre , String apellidos, int edad){
-	   this.id = id;
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-		this.edad = edad;
-	}
+    protected int id;
+    protected String nombre;
+    protected String apellidos;
+    protected int edad;
+    
+      public SeleccionFutbol(int id,String nombre , String apellidos, int edad){
+       this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
 
 
-	public void Concentrarse() {
-	
-	}
+    public void concentrarse() {
+        System.out.print("el plantel completo se concentro en el hotel");
+    }
 
-	public void Viajar() {
-		
-	}
+    public void viajar() {
+                System.out.print("el plantel volvio al pais, eliminado :(");
+
+    }
+    
+    public void pruebaCovid(){
+                System.out.print("nuestro unico buen jugador salio positivo, vaya suerte");
+
+    }
 }
