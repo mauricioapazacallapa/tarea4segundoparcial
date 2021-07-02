205472982ebd1810d69480f489fb59c39bdb7941
mauricioapazacallapa <81927407+mauricioapazacallapa@users.noreply.github.com>
2021-07-02 00:54:31 -0400

    Update Entrenador.java

--- Entrenador.java
+++ Entrenador.java
@@ -1,23 +1,23 @@
 
 public class Entrenador extends SeleccionFutbol
 {
-
-	private String idFederacion;
+    private boolean fuejugador;
+    private String idFederacion;
         public Entrenador(String idFederacion,int id, String nombre, String apellidos, int edad) {
-		super(id,nombre, apellidos, edad);
-		this.idFederacion=idFederacion;
-	}
+        super(id,nombre, apellidos, edad);
+        this.idFederacion=idFederacion;
+    }
 
 
-	public String dirigirPartido() {
-	    String resul="";
-	     if(edad<60){
-	         resul="dirigio el partido con normalidad";
-			 
-	     }else{
-	       resul="esta muy mayor, busqye otro DT";
-	       }
-	       return resul;
-	}
+    public String dirigirPartido() {
+        String resul="";
+         if(edad<60){
+             resul="dirigio el partido con normalidad";
+             
+         }else{
+           resul="esta muy mayor, busque otro DT";
+           }
+           return resul;
+    }
 
 }
